3fdfa547ab363fe842cbb56bd835f77f0bf6a12f
miroslavpokorny98 <miroslav.pokorny.98@gmail.com>
2022-04-18 09:28:45 +0200

    pricing section animation done

--- src/blocks/09_pricing/_pricing.js
+++ src/blocks/09_pricing/_pricing.js
@@ -42,3 +42,42 @@ gsap.to(".pricing", {
         // markers: true,
     }
 });
+
+const expandBtn = document.querySelector(".card4 .expand")
+const expandBtnCross = document.querySelector(".card4 .expand img")
+const card4Middle = document.querySelector(".card4Middle")
+const pricingNumbers = document.querySelector(".pricingNumbers")
+const card4MiddleChecks = document.querySelector(".card4MiddleChecks")
+
+
+const startWidth = card4Middle.style.width
+// const startWidth = 440
+
+expandBtn.onclick = expandPricing
+
+
+function expandPricing () {
+    // card4Middle.classList.add("mystyle");
+    // card4Middle.style.width = "100%"
+    expandBtn.onclick = retractPricing
+    // expandBtnCross.style.transform = rotate(0.5turn)
+    gsap.to(".card4 .expand img", {rotation: 45})
+    gsap.to(".card4Middle", {width: "100%", duration: 1, ease: "inOut"})
+    pricingNumbers.style.display = "block"
+    gsap.from(".pricingNumbers", {opacity: 0, duration: 1})
+    card4MiddleChecks.style.width = "50%"
+    
+}
+
+function retractPricing () {
+    // card4Middle.classList.add("mystyle");
+    // card4Middle.style.width = "440px"
+    expandBtn.onclick = expandPricing
+    gsap.to(".card4 .expand img", {rotation: 0})
+    gsap.to(".card4Middle", {width: startWidth, duration: 1})
+    pricingNumbers.style.display = "none"
+    card4MiddleChecks.style.width = "100%"
+}
+
+
+
